474460adc9f83eeeb8cfad88fe53bad9b7510ab1
MallisOG <mallisiscool@gmail.com>
2019-08-03 03:17:04 -0400

    Start sponge support

--- src/com/DisabledMallis/HybridLib/Hybrid.java
+++ src/com/DisabledMallis/HybridLib/Hybrid.java
@@ -1,6 +1,8 @@
 package com.DisabledMallis.HybridLib;
 
 import org.bukkit.Bukkit;
+import org.spongepowered.api.Platform;
+import org.spongepowered.api.Sponge;
 
 /*
  * Hybrid detection class made by DisabledMallis
@@ -14,47 +16,75 @@ import org.bukkit.Bukkit;
    }
  */
 public class Hybrid {
+	static boolean BukkitAPI;
+	static boolean SpongeAPI;
 	static boolean Kettle;
 	static boolean Lava14;
 	static boolean Lava;
 	
 	public static void enableLib() {
-		new Log("Loading ForgeBukkitLib...");
-		new Log("Server version: " + Bukkit.getVersion());
-		new Log("Bukkit version: " + Bukkit.getBukkitVersion());
+		new Log("Loading HybridLib...");
+		try {
+			Class.forName("org.bukkit.Bukkit");
+			new Log("Detected Bukkit!");
+			BukkitAPI = true;
+			new Log("Server version: " + Bukkit.getVersion());
+			new Log("Bukkit version: " + Bukkit.getBukkitVersion());
+		} catch (ClassNotFoundException e) {
+			new Log("Couldn't find Bukkit API");
+		}
+		try {
+			Class.forName("org.spongepowered.api.Sponge");
+			new Log("Detected Sponge!");
+			SpongeAPI=true;
+			new Log("Server version: " + Sponge.getPlatform().getMinecraftVersion().getName());
+			Sponge.getPlatform();
+			new Log("Sponge version: " + Platform.API_ID);
+		} catch (ClassNotFoundException e) {
+			new Log("Couldn't find Sponge API");
+		}
+		
 		try {
 			Class.forName("net.minecraftforge.common.ForgeVersion");
 			new Log("Found Forge!");
-			new Log("Checking for Kettle...");
-			try {
-				Class.forName("kettlefoundation.kettle.KettleVersionCommand");
-				new Log("Kettle found!");
-				Kettle = true;
+			if(BukkitAPI) {
+				new Log("Checking for Kettle...");
+				try {
+					Class.forName("kettlefoundation.kettle.KettleVersionCommand");
+					new Log("Kettle found!");
+					Kettle = true;
+				}
+				catch(ClassNotFoundException ex) {
+					new Log("Couldn't find Kettle.");
+					Kettle = false;
+				}
+				new Log("Checking for LavaPowered 1.14.4...");
+				try {
+					Class.forName("org.lavapowered.lava.LavaInternal");
+					new Log("LavaPowered 1.14.4 found!");
+					Lava14 = true;
+				}
+				catch(ClassNotFoundException ex) {
+					new Log("Couldn't find LavaPowered 1.14.4.");
+					Lava14 = false;
+				}
+				new Log("Checking for Lava...");
+				if(Bukkit.getVersion().contains("Lava")) {
+					new Log("LavaPowered found!");
+					Lava = true;
+				} else {
+					new Log("Couldn't find LavaPowered.");
+					Lava = false;
+				}
+				if(Kettle == false && Lava14 == false && Lava == false) {
+					new Log("Unknown Forge Bukkit hybrid.");
+				}
 			}
-			catch(ClassNotFoundException ex) {
-				new Log("Couldn't find Kettle.");
-				Kettle = false;
+			else if(SpongeAPI) {
+				new Log("This is likely SpongeForge.");
 			}
-			new Log("Checking for LavaPowered 1.14.4...");
-			try {
-				Class.forName("org.lavapowered.lava.LavaInternal");
-				new Log("LavaPowered 1.14.4 found!");
-				Lava14 = true;
-			}
-			catch(ClassNotFoundException ex) {
-				new Log("Couldn't find LavaPowered 1.14.4.");
-				Lava14 = false;
-			}
-			new Log("Checking for Lava...");
-			if(Bukkit.getVersion().contains("Lava")) {
-				new Log("LavaPowered found!");
-				Lava = true;
-			} else {
-				new Log("Couldn't find LavaPowered.");
-				Lava = false;
-			}
-			if(Kettle == false && Lava14 == false && Lava == false) {
-				new Log("Unknown Forge Bukkit hybrid.");
+			else {
+				new Log("Odd hybrid, can't identify.");
 			}
 		}
 		catch(ClassNotFoundException ex) {
@@ -78,6 +108,13 @@ public class Hybrid {
 				return Lava14;
 			}
 			return false;
+		}
+		
+		public boolean isBukkit() {
+			return BukkitAPI;
+		}
+		public boolean isSponge() {
+			return SpongeAPI;
 		}
 	}
 	
